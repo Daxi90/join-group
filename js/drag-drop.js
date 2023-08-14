bcca14a939eb10eb86939ebc95960e6b23e87bd3
Daxi90 <hdateam90@gmail.com>
2023-08-14 14:40:27 +0200

    fixed drag and drop

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -6,10 +6,22 @@ function startDragging(id){
 
 function allowDrop(ev) {
     ev.preventDefault();
+
 };
 
 function moveTo(newstatus){
     tasks[currentDraggedElement].status = newstatus;
+    document.getElementById('todoBoard').classList.remove('onDragOver');
+    document.getElementById('inProgressBoard').classList.remove('onDragOver');
+    document.getElementById('awaitFeedBackBoard').classList.remove('onDragOver');
+    document.getElementById('doneBoard').classList.remove('onDragOver');
     kanbanInit();
 };
 
+function highlight(element){
+    document.getElementById(element).classList.add('onDragOver');
+}
+
+function removeHighlight(element){
+    document.getElementById(element).classList.remove('onDragOver');
+}
