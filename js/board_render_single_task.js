b474edc024c193f37ddd93d695e7af0a0ca3c00c
Daxi90 <hdateam90@gmail.com>
2023-09-18 10:11:19 +0200

    Fixed Subtask

--- js/board_render_single_task.js
+++ js/board_render_single_task.js
@@ -124,7 +124,7 @@ function removeTask(id) {
  * @param {string} subtaskIdStr - The ID of the subtask (as a string) that was clicked.
  */
 function clickSubTask(subtaskIdStr) {
-  const subtaskId = parseFloat(subtaskIdStr); // Konvertiert die ID in eine Zahl
+  const subtaskId = subtaskIdStr; // Konvertiert die ID in eine Zahl
 
   // Finden Sie die übergeordnete Aufgabe, die diesen Subtask enthält
   const parentTask = tasks.find(
@@ -156,12 +156,227 @@ function clickSubTask(subtaskIdStr) {
 
 //------------------------------------RENDER EDIT FORM----------------------------------------
 function renderEditForm(taskId, containerId) {
-  
   const container = document.querySelector(containerId);
   if (!container) return;
 
-  // Ihr Formular-HTML hier
-  const formHTML = /*html*/`
+  container.innerHTML = getFormHTML(taskId);
+  // Binden Sie die Event-Listener und andere Initialisierungsfunktionen
+  taskFormJS();
+
+  if (taskId !== undefined && taskId !== null) {
+    const taskData = getTaskData(taskId);
+    fillFormWithData(taskData);
+  }
+}
+
+function getTaskData(taskId) {
+  // Hier können Sie die Daten für die gegebene Task-ID abrufen.
+  // Zum Beispiel aus einem Array oder von einer API.
+  return tasks.find((task) => task.id === taskId);
+}
+
+function fillFormWithData(taskData) {
+  // Titel und Beschreibung
+  document.getElementById("title").value = taskData.title;
+  document.getElementById("description").value = taskData.description;
+
+  // Fälligkeitsdatum
+  document.getElementById("duedate").value = taskData.completionDate;
+
+  // Priorität
+  selectPriorityButton(taskData.priority);
+
+  // Dropdown für zugewiesene Kontakte füllen
+  const optionsContainer = document.getElementById("options");
+  optionsContainer.innerHTML = ""; // Löschen Sie zuerst den aktuellen Inhalt
+
+  for (const contact of contacts) {
+    const isChecked = taskData.assignedPersons.includes(contact.id)
+      ? "checked"
+      : "";
+    const optionDiv = document.createElement("div");
+    optionDiv.className = "option";
+
+    const contactLineDiv = document.createElement("div");
+    contactLineDiv.className = "contactLine";
+
+    const initialsDiv = document.createElement("div");
+    initialsDiv.className = "initials";
+    initialsDiv.setAttribute("data-contact-id", contact.id);
+    initialsDiv.style.backgroundColor = contact.color;
+    initialsDiv.textContent = contact.initials;
+
+    const nameSpan = document.createElement("span");
+    nameSpan.className = "name";
+    nameSpan.textContent = contact.name;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    if (isChecked) checkbox.checked = true;
+
+    checkbox.addEventListener("change", function () {
+      if (this.checked) {
+        // Fügen Sie die ID des Kontakts zur Liste der zugewiesenen Personen hinzu
+        taskData.assignedPersons.push(contact.id);
+      } else {
+        // Entfernen Sie die ID des Kontakts aus der Liste der zugewiesenen Personen
+        const index = taskData.assignedPersons.indexOf(contact.id);
+        if (index > -1) {
+          taskData.assignedPersons.splice(index, 1);
+        }
+      }
+      updateSelectedContacts(taskData.assignedPersons);
+    });
+
+    contactLineDiv.appendChild(initialsDiv);
+    contactLineDiv.appendChild(nameSpan);
+    optionDiv.appendChild(contactLineDiv);
+    optionDiv.appendChild(checkbox);
+    optionsContainer.appendChild(optionDiv);
+  }
+
+  // Zugewiesene Kontakte im "selected-contacts"-Container anzeigen
+  updateSelectedContacts(taskData.assignedPersons);
+
+  // Kategorie
+  const categorySelect = document.querySelector(
+    ".category-select .selected-option"
+  );
+  if (categorySelect) {
+    categorySelect.textContent = taskData.category.name;
+  }
+
+  // Subtasks
+  const subtasksList = document.querySelector(".subtasks-list");
+  let subtasksHTML = "";
+  for (const subtask of taskData.subtasks) {
+    const isChecked = subtask.completed ? "checked" : "";
+    subtasksHTML += `
+    <li class="subtask-item">
+      <input type="checkbox" id="subtask-${subtask.id}" ${isChecked}>
+      <label for="subtask-${subtask.id}">${subtask.title}</label>
+      <button onclick="deleteSubtask('${subtask.id}', '${taskData.id}')">Löschen</button>
+    </li>`;
+  }
+  subtasksList.innerHTML = subtasksHTML;
+}
+
+function updateSelectedContacts(assignedPersons) {
+  const selectedContactsContainer =
+    document.querySelector(".selected-contacts");
+  let selectedContactsHTML = "";
+  for (const assignedPersonId of assignedPersons) {
+    const contact = contacts.find((c) => c.id === assignedPersonId);
+    if (contact) {
+      selectedContactsHTML += `
+        <div class="single-task-assigned-contacts">
+          <span class="single-task-assignee" style="background: ${contact.color}">${contact.initials}</span>
+          <span>${contact.name}</span>
+        </div>`;
+    }
+  }
+  selectedContactsContainer.innerHTML = selectedContactsHTML;
+}
+
+function saveEditedTaskData(taskId) {
+  const task = tasks.find((t) => t.id === taskId);
+  if (!task) {
+    console.error(`Task with ID ${taskId} not found.`);
+    return;
+  }
+
+  // Titel und Beschreibung
+  task.title = document.getElementById("title").value;
+  task.description = document.getElementById("description").value;
+
+  // Fälligkeitsdatum
+  task.completionDate = document.getElementById("duedate").value;
+
+  // Priorität
+  const priorityButtons = document.querySelectorAll(".prioButton");
+  for (const button of priorityButtons) {
+    if (button.classList.contains("selected")) {
+      task.priority = button.textContent.trim().toLowerCase();
+      break;
+    }
+  }
+
+  // Zugewiesene Kontakte
+  const contactCheckboxes = document.querySelectorAll(
+    '#options .option input[type="checkbox"]'
+  );
+  task.assignedPersons = [];
+  contactCheckboxes.forEach((checkbox, index) => {
+    if (checkbox.checked) {
+      task.assignedPersons.push(contacts[index].id);
+    }
+  });
+
+  // Kategorie
+  const selectedCategory = document
+    .querySelector(".category-select .selected-option")
+    .textContent.trim();
+  task.category = {
+    name: selectedCategory,
+    // Hier setzen wir den Hintergrund auf einen Standardwert. Sie können dies anpassen, um den tatsächlichen Wert zu erhalten.
+    backgroundColor: "#ff0000",
+  };
+
+  // Subtasks
+  let subtasks = Array.from(document.querySelectorAll('.subtask-item'))
+  .map((option, index) => ({
+      id: `${tasks.length}.${index + 1}`,
+      title: option.textContent.trim().replace('● ', ''),
+      completed: false
+  }));
+
+task.subtasks = subtasks;
+
+  // Hier können Sie weitere Schritte hinzufügen, z. B. das Aktualisieren des `tasks`-Arrays in Ihrem lokalen Speicher oder das Senden an einen Server.
+  setItem("tasks", tasks);
+  closeTaskCard();
+  kanbanInit(tasks);
+}
+
+function selectPriorityButton(priority) {
+  let buttonClass;
+  switch (priority) {
+    case "high":
+    case "urgent":
+      buttonClass = ".prioUrgent";
+      break;
+    case "medium":
+      buttonClass = ".prioMedium";
+      break;
+    case "low":
+      buttonClass = ".prioLow";
+      break;
+  }
+  if (buttonClass) {
+    const targetButton = document.querySelector(buttonClass);
+    if (targetButton) {
+      // Alle Prioritätsbuttons von der Klasse 'selected' bereinigen
+      const allPrioButtons = document.querySelectorAll(".prioButton");
+      allPrioButtons.forEach((btn) => btn.classList.remove("selected"));
+
+      // Klasse 'selected' zum ausgewählten Button hinzufügen
+      targetButton.classList.add("selected");
+    }
+  }
+}
+
+function deleteSubtask(subtaskId, taskId) {
+  const task = tasks.find((t) => t.id == taskId);
+  const index = task.subtasks.findIndex((st) => st.id === subtaskId);
+  if (index > -1) {
+    task.subtasks.splice(index, 1);
+  }
+  fillFormWithData(task);  // Formular neu laden
+}
+
+
+function getFormHTML(taskId) {
+  return /*html*/ `
   <div class="taskwidth">
 <div>
   <input required type="text" placeholder="Enter a title" id="title" class="titleInput">
@@ -241,210 +456,7 @@ function renderEditForm(taskId, containerId) {
            
 </div>
 <div class="submit">       
-<button onclick="saveEditedTaskData(${taskId})" class="createTask">Save <img src="/assets/img/check.svg" alt=""></button>
+<button onclick="saveEditedTaskData(${taskId})" class="saveTaskBtn">Save <img src="/assets/img/check.svg" alt=""></button>
 </div>
-`;
-
-  container.innerHTML = formHTML;
-
-  // Binden Sie die Event-Listener und andere Initialisierungsfunktionen
-  taskFormJS();
-
-  if (taskId !== undefined && taskId !== null) {
-    const taskData = getTaskData(taskId);
-    fillFormWithData(taskData);
-}
-
-}
-
-function getTaskData(taskId) {
-  // Hier können Sie die Daten für die gegebene Task-ID abrufen.
-  // Zum Beispiel aus einem Array oder von einer API.
-  return tasks.find(task => task.id === taskId);
-}
-
-
-function fillFormWithData(taskData) {
-  // Titel und Beschreibung
-  document.getElementById('title').value = taskData.title;
-  document.getElementById('description').value = taskData.description;
-
-  // Fälligkeitsdatum
-  document.getElementById('duedate').value = taskData.completionDate;
-
-  // Priorität
-  selectPriorityButton(taskData.priority);
-
-  // Dropdown für zugewiesene Kontakte füllen
-  const optionsContainer = document.getElementById('options');
-  optionsContainer.innerHTML = ''; // Löschen Sie zuerst den aktuellen Inhalt
-
-  for (const contact of contacts) {
-    const isChecked = taskData.assignedPersons.includes(contact.id) ? 'checked' : '';
-    const optionDiv = document.createElement('div');
-    optionDiv.className = 'option';
-
-    const contactLineDiv = document.createElement('div');
-    contactLineDiv.className = 'contactLine';
-
-    const initialsDiv = document.createElement('div');
-    initialsDiv.className = 'initials';
-    initialsDiv.setAttribute('data-contact-id', contact.id);
-    initialsDiv.style.backgroundColor = contact.color;
-    initialsDiv.textContent = contact.initials;
-
-    const nameSpan = document.createElement('span');
-    nameSpan.className = 'name';
-    nameSpan.textContent = contact.name;
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    if (isChecked) checkbox.checked = true;
-
-    checkbox.addEventListener('change', function() {
-      if (this.checked) {
-        // Fügen Sie die ID des Kontakts zur Liste der zugewiesenen Personen hinzu
-        taskData.assignedPersons.push(contact.id);
-      } else {
-        // Entfernen Sie die ID des Kontakts aus der Liste der zugewiesenen Personen
-        const index = taskData.assignedPersons.indexOf(contact.id);
-        if (index > -1) {
-          taskData.assignedPersons.splice(index, 1);
-        }
-      }
-      updateSelectedContacts(taskData.assignedPersons);
-    });
-
-    contactLineDiv.appendChild(initialsDiv);
-    contactLineDiv.appendChild(nameSpan);
-    optionDiv.appendChild(contactLineDiv);
-    optionDiv.appendChild(checkbox);
-    optionsContainer.appendChild(optionDiv);
-  }
-
-  // Zugewiesene Kontakte im "selected-contacts"-Container anzeigen
-  updateSelectedContacts(taskData.assignedPersons);
-
-  // Kategorie
-  const categorySelect = document.querySelector('.category-select .selected-option');
-  if (categorySelect) {
-    categorySelect.textContent = taskData.category.name;
-  }
-
-  // Subtasks
-  const subtasksList = document.querySelector('.subtasks-list');
-  let subtasksHTML = '';
-  for (const subtask of taskData.subtasks) {
-    const isChecked = subtask.completed ? 'checked' : '';
-    subtasksHTML += `
-      <li class="subtask-item">
-        <input type="checkbox" id="subtask-${subtask.id}" ${isChecked}>
-        <label for="subtask-${subtask.id}">${subtask.title}</label>
-      </li>`;
-  }
-  subtasksList.innerHTML = subtasksHTML;
-}
-
-function updateSelectedContacts(assignedPersons) {
-  const selectedContactsContainer = document.querySelector('.selected-contacts');
-  let selectedContactsHTML = '';
-  for (const assignedPersonId of assignedPersons) {
-    const contact = contacts.find(c => c.id === assignedPersonId);
-    if (contact) {
-      selectedContactsHTML += `
-        <div class="single-task-assigned-contacts">
-          <span class="single-task-assignee" style="background: ${contact.color}">${contact.initials}</span>
-          <span>${contact.name}</span>
-        </div>`;
-    }
-  }
-  selectedContactsContainer.innerHTML = selectedContactsHTML;
-}
-
-
-
-
-function saveEditedTaskData(taskId) {
-  const task = tasks.find(t => t.id === taskId);
-  if (!task) {
-    console.error(`Task with ID ${taskId} not found.`);
-    return;
-  }
-
-  // Titel und Beschreibung
-  task.title = document.getElementById('title').value;
-  task.description = document.getElementById('description').value;
-
-  // Fälligkeitsdatum
-  task.completionDate = document.getElementById('duedate').value;
-
-  // Priorität
-  const priorityButtons = document.querySelectorAll('.prioButton');
-  for (const button of priorityButtons) {
-    if (button.classList.contains('selected')) {
-      task.priority = button.textContent.trim().toLowerCase();
-      break;
-    }
-  }
-
-  // Zugewiesene Kontakte
-  const contactCheckboxes = document.querySelectorAll('#options .option input[type="checkbox"]');
-  task.assignedPersons = [];
-  contactCheckboxes.forEach((checkbox, index) => {
-    if (checkbox.checked) {
-      task.assignedPersons.push(contacts[index].id);
-    }
-  });
-
-  // Kategorie
-  const selectedCategory = document.querySelector('.category-select .selected-option').textContent.trim();
-  task.category = {
-    name: selectedCategory,
-    // Hier setzen wir den Hintergrund auf einen Standardwert. Sie können dies anpassen, um den tatsächlichen Wert zu erhalten.
-    backgroundColor: '#ff0000'
-  };
-
-  // Subtasks
-  const subtaskCheckboxes = document.querySelectorAll('.subtasks-list input[type="checkbox"]');
-  subtaskCheckboxes.forEach((checkbox, index) => {
-    if (task.subtasks[index]) {
-      task.subtasks[index].completed = checkbox.checked;
-    }
-  });
-
-  // Hier können Sie weitere Schritte hinzufügen, z. B. das Aktualisieren des `tasks`-Arrays in Ihrem lokalen Speicher oder das Senden an einen Server.
-  setItem('tasks', tasks);
-  closeTaskCard();
-  kanbanInit(tasks);
-}
-
-
-
-function selectPriorityButton(priority) {
-  let buttonClass;
-  switch (priority) {
-      case 'high':
-          buttonClass = '.prioUrgent';
-          break;
-      case 'urgent':
-          buttonClass = '.prioUrgent';
-          break;
-      case 'medium':
-          buttonClass = '.prioMedium';
-          break;
-      case 'low':
-          buttonClass = '.prioLow';
-          break;
-  }
-  if (buttonClass) {
-      const targetButton = document.querySelector(buttonClass);
-      if (targetButton) {
-          // Alle Prioritätsbuttons von der Klasse 'selected' bereinigen
-          const allPrioButtons = document.querySelectorAll('.prioButton');
-          allPrioButtons.forEach(btn => btn.classList.remove('selected'));
-
-          // Klasse 'selected' zum ausgewählten Button hinzufügen
-          targetButton.classList.add('selected');
-      }
-  }
+    `;
 }
