ab09fc0d54b63452d5b909cefa211294b0ef1077
Eldras1 <jens.labudda@hotmail.de>
2023-09-21 23:33:44 +0200

    update 2

--- js/register.js
+++ js/register.js
@@ -1,34 +1,33 @@
 let users = [];
 
-async function loadUsers(){
+async function loadUsers() {
     try {
         users = JSON.parse(await getItem('users'));
-    } catch(e){
+    } catch (e) {
         console.error('Loading error:', e);
     }
 }
 
 async function register() {
-   /*  users = await getItem('users'); */
+    /*  users = await getItem('users'); */
+    document.getElementById('signUpMessage').classList.remove('d-none');
     let signUpUser = document.getElementById('signUpUser');
     let signUpMail = document.getElementById('signUpMail');
     let signUpPassword = document.getElementById('signUpPassword');
+    let confirmPassword = document.getElementById('signUpConfirmpassword');
     users.push({
         user: signUpUser.value,
         mail: signUpMail.value,
         password: signUpPassword.value
     })
 
-    await setItem('users', JSON.stringify(users));
-
-    resetForm();
-    
-}
-
-function resetForm() {
     signUpUser.value = '';
     signUpMail.value = '';
     signUpPassword.value = '';
+    confirmPassword.value = '';
+
+    await setItem('users', JSON.stringify(users));
+    setTimeout(()=>{window. location = 'login.html' },3000);
 }
 
 function checkPrivacyPolicy() {
