bdd81980fa35c7f8f928c96b16dbd282d9c0dbe1
Daxi90 <hdateam90@gmail.com>
2023-09-21 17:12:59 +0200

    Changed login.js

--- js/login.js
+++ js/login.js
@@ -1,20 +1,28 @@
-function login() {
-    event.preventDefault();
+function login(event) {
+    //event.preventDefault(); 
     let mail = document.getElementById('email');
-    mail.setCustomValidity('');
     let password = document.getElementById('password');
-    let user = users.find( u => u.mail == mail.value && u.password == password.value);
-    console.log(user);
-    if (user) {
-        console.log('User gefunden');
 
-        window.location.href="board.html";
-    }
-    else {
+    mail.setCustomValidity('');
+
+    let user = users.find(u => u.mail === mail.value && u.password === password.value);
+    
+    if (user) {
+        window.location.href = "board.html";
+    } else {
         mail.setCustomValidity("Your password or your Email has been incorrect");
         mail.reportValidity();
+
+        // Verzögerung, um die benutzerdefinierte Validität zurückzusetzen
+        setTimeout(() => {
+            mail.setCustomValidity('');
+        }, 1000);
     }
 }
+
+
+
+
 
 function rememberMe() {
     document.getElementById('loginCheckmark').classList.add('d-none');
