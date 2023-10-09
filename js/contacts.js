9d12d0940e7b4a151dfd8a25cbc88b60f10be510
Eldras1 <jens.labudda@hotmail.de>
2023-10-09 16:57:20 +0200

    form validation for email on phone new contact

--- js/contacts.js
+++ js/contacts.js
@@ -276,6 +276,34 @@ function closePopUpsOnClickOverlay() {
     closeEditContact();
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const inputs = Array.from(
+      document.querySelectorAll('input[name=email], input[name=phone]')
+    );
+  
+    const inputListener = e => {
+      inputs
+        .filter(i => i !== e.target)
+        .forEach(i => (i.required = !e.target.value.length));
+    };
+  
+    inputs.forEach(i => i.addEventListener('input', inputListener));
+  });
+
+/*   document.addEventListener('DOMContentLoaded', function() {
+    const inputs = Array.from(
+      document.querySelectorAll('input[name=email], input[name=phone]')
+    );
+  
+    const inputListener = e => {
+      inputs
+        .filter(i => i !== e.target)
+        .forEach(i => (i.required = !e.target.value.length));
+    };
+  
+    inputs.forEach(i => i.addEventListener('input', inputListener));
+  }); */
+
 document.addEventListener('DOMContentLoaded', function(){
     getContacts();
     getTasks();
